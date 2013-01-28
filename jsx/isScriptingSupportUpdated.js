3434fe9e589b96b31f987708c1716b5ea1740e5e
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2013-01-28 19:13:20 +0100

    plugin architecture draft

--- jsx/isScriptingSupportUpdated.js
+++
@@ -1,5 +0,0 @@
-var minimumBuildDate = new Date('Aug  8 2012 13:48:46')
-var currentBuildDate = new Date(app.scriptingBuildDate)
-
-// returning value
-JSON.stringify(minimumBuildDate <= currentBuildDate)
--- server.js
+++ server.js
@@ -1,62 +1,25 @@
 var http = require('http'),
     net = require('net'),
     fs = require('fs'),
-    socketio = require('socket.io'),
-    express = require('express'),
-    cocoa = require('./lib/cocoa')(15000),
-    motherlover = require('./lib/motherlover')
+    argv = require('commander'),
+    Generator = require('./lib/generator')
 
-// PS connection
+argv
+  .option('-p, --port <s>', 'The Photoshop server port')
+  .option('-H, --host <s>', 'The Photoshop server host')
+  .option('-P, --password <s>', 'The Photoshop server password')
+  .parse(process.argv)
 
-ml.use()
+var generator = new Generator()
 
-
-ml.on('error', function (err, response) {
-  process.cocoaSend({ name: 'error', data: {
-    message: err.message,
-    stack: err.stack,
-    response: response,
-    code: err.code
-  } })
-})
-
-
-// http server
-var app = express()
-var server = http.createServer(app)
-var sio = socketio.listen(server, { log: false })
-var ml = motherlover(app, sio)
-
-// get a random available port
-net.createServer().once('close', function() {
-  server.listen(this.availablePort)
-  process.cocoaSend({ name: 'httpPort', data: this.availablePort })
-}).once('listening', function() {
-  this.availablePort = this.address().port
-  this.close()
-}).listen(0)
-
-process.on('cocoaMessage', function (message) {
-  message.name !== 'ping' && process.cocoaSend({ name: 'back', message: message })
-  switch (message.name) {
-    case 'connect':
-      ml.connect(message.host, message.port, message.password, 500, function (err) {
-        if (!err) {
-          process.cocoaSend({ name: 'connected' })
-        } else {
-          throw err
-        }
-      })
-    break
-    case 'disconnect':
-      ml.close()
-      process.cocoaSend({ name: 'disconnected' })
-    break
-  }
+generator.connect(argv.host, argv.port, argv.password, 500, function (err) {
+  if (err) throw err
+  generator.load()
 })
 
 process.on('uncaughtException', function (err) {
+  // Report remotely?
   console.error(err, err.stack)
-  process.cocoaSend({ name: 'error', data: { message: err.message, stack: err.stack, code: 'UNCAUGHT_EXCEPTION' } })
+  console.error('Killing generator')
   process.kill()
 })
