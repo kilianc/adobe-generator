4bd384e5f89016a63d8ebc1c2e7e7e2ae752cfae
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2013-01-29 02:16:46 +0100

    removed useless files

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ generator.connect(argv.host, argv.port, argv.password, 500, function (err) {
 
 process.on('uncaughtException', function (err) {
   // Report remotely?
-  console.error(err, err.stack)
-  console.error('Killing generator')
+  console.error(err.stack)
+  console.error({ name: 'error', data: { message: err.message, stack: err.stack, code: 'UNCAUGHT_EXCEPTION' } })
   process.kill()
 })
