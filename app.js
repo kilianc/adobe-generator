9ae34952ec5a8b788f0a799c93a36c53154fd3bc
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2013-01-30 10:13:49 +0100

    closes #96 - add -use and -nuse cli options

--- app.js
+++ app.js
@@ -2,20 +2,30 @@
 var http = require('http'),
     net = require('net'),
     fs = require('fs'),
-    argv = require('commander'),
-    Generator = require('./lib/generator')
+    program = require('commander'),
+    Generator = require('./lib/generator'),
+    version = require('./package.json').version
 
-argv
+program
+  .version(version)
   .option('-p, --port <s>', 'The Photoshop server port', 49494)
   .option('-H, --host <s>', 'The Photoshop server host', 'localhost')
   .option('-P, --password <s>', 'The Photoshop server password', 'password')
+  .option('-t, --timeout <ms>', 'Time before timeout', 500)
+  .option('-u, --use \'<name> <name> ...\'', 'Explicitly activate plugins')
+  .option('-U, --nuse \'<name> <name> ...\'', 'Explicitly disable plugins', '')
   .parse(process.argv)
 
 var generator = new Generator()
 
-generator.connect(argv.host, argv.port, argv.password, 500, function (err) {
+generator.connect(program.host, program.port, program.password, program.timeout, function (err) {
   if (err) throw err
-  generator.load()
+
+  var enablePlugins = program.use ? program.use.split(' ') : false
+  var disablePlugins = program.nuse ? program.nuse.split(' ') : []
+  var activePlugins = generator.loadPlugins(enablePlugins, disablePlugins)
+
+  console.log('\n - Correctly activated %d plugins: %j\n', activePlugins.length, activePlugins)
 })
 
 process.on('uncaughtException', function (err) {
